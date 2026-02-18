ffa5cff800270b81bf5e2298def9ee84a66d79a2
Markus Haegele <markus@hhaegele.de>
2026-02-18 11:42:43 +0100

    changing initscript

--- pull_esp-idf.sh
+++ pull_esp-idf.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 exec git clone -b v5.1.7 --recursive https://github.com/espressif/esp-idf.git esp-idf-v5.1.7
+cd esp-idf*
+./install.sh esp32
+cp -r esp-idf*/examples/get-started/hello_world .
